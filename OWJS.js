e12dc08f8374746f98619353df88a415e4154283
Andax <andaxhambert@gmail.com>
2016-05-05 18:43:23 +1000

    Add document.ready(function() { });

--- OWJS.js
+++ OWJS.js
@@ -1,19 +1,21 @@
-$('.switch').hover(function() {
-        $(this).find('.hero').hide();
-        $(this).find('.counter').show();
-    }, function() {
-        $(this).find('.counter').hide();
-        $(this).find('.hero').show();
-});
+$(document).ready(function() {
+    $('.switch').hover(function() {
+            $(this).find('.hero').hide();
+            $(this).find('.counter').show();
+        }, function() {
+            $(this).find('.counter').hide();
+            $(this).find('.hero').show();
+    });
 
-$(document).scroll(function() {
-    var fromTopPx = 500; // distance to trigger
-    var scrolledFromtop = $(document).scrollTop();
-    if(scrolledFromtop > fromTopPx){
-        $('.hero-image').css('background-image', 'url(https://i.imgur.com/9ll4Kmc.jpg)');
-    }
-});
+    $(document).scroll(function() {
+        var fromTopPx = 500; // distance to trigger
+        var scrolledFromtop = $(document).scrollTop();
+        if(scrolledFromtop > fromTopPx){
+            $('.hero-image').css('background-image', 'url(https://i.imgur.com/9ll4Kmc.jpg)');
+        }
+    });
 
-$('.hero-image').click(function() {
-    $(this).css('background-image', 'url(https://wallpaperspal.com/wp-content/uploads/Overwatch-2015-Game-Logo-Wallpaper.jpg)');
+    $('.hero-image').click(function() {
+        $(this).css('background-image', 'url(https://wallpaperspal.com/wp-content/uploads/Overwatch-2015-Game-Logo-Wallpaper.jpg)');
+    });
 });
